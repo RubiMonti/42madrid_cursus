6d95b1c7bc730b7dea2d9cedd57b8a471f384399
Ruben Montilla <rmontill@c1r13s1.42madrid.com>
2020-07-15 10:20:41 +0200

    Empezado el printf

---
+++ printf/libft/ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calloc.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmontill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/07 16:26:29 by rmontill          #+#    #+#             */
+/*   Updated: 2019/11/27 16:39:10 by rmontill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+	size_t	i;
+
+	i = 0;
+	ptr = malloc(size * count);
+	if (ptr == 0)
+		return (0);
+	while (i < count * size)
+	{
+		((char *)ptr)[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
